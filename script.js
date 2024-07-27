19c75849035576fc8b85c6d3e5fde23e9bb80206
Shimaa-Awed <152649438+Shimaa-Awed@users.noreply.github.com>
2024-07-27 06:43:34 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const imageContainer = document.querySelector('.image-container');
+const images = imageContainer.querySelectorAll('img');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  });
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+});
+
+showImage(currentIndex);
